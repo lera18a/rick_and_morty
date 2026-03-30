84ea1068a69bc62276db99460fd555640bfc324b
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 13:28:40 +0300

    feat(domain/entity): added CharacterData model

---
+++ lib/domain/entity/character_data.dart
@@ -0,0 +1,83 @@
+// можно потом сделать отдельно
+// location - model и origin - модель
+import 'dart:convert';
+
+class CharacterData {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String locationName;
+  final String locationUrl;
+  final String originName;
+  final String originUrl;
+  final String image;
+  final List<String> episodes;
+  final String url;
+  final String created;
+  final int cachedAt;
+
+  const CharacterData({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.locationName,
+    required this.locationUrl,
+    required this.originName,
+    required this.originUrl,
+    required this.image,
+    required this.episodes,
+    required this.url,
+    required this.created,
+    required this.cachedAt,
+  });
+
+  // в базу
+  Map<String, dynamic> toDatabase() {
+    return {
+      'id': id,
+      'name': name,
+      'status': status,
+      'species': species,
+      'type': type,
+      'gender': gender,
+      'location_name': locationName,
+      'location_url': locationUrl,
+      'origin_name': originName,
+      'origin_url': originUrl,
+      'image': image,
+      'episodes': jsonEncode(episodes),
+      'url': url,
+      'created': created,
+      'cached_at': cachedAt,
+    };
+  }
+
+  // из базы
+  factory CharacterData.fromDatabase(Map<String, dynamic> data) {
+    return CharacterData(
+      id: data['id'] as int,
+      name: data['name'] as String,
+      status: data['status'] as String,
+      species: data['species'] as String,
+      type: data['type'] as String,
+      gender: data['gender'] as String,
+      locationName: data['location_name'] as String,
+      locationUrl: data['location_url'] as String,
+      originName: data['origin_name'] as String,
+      originUrl: data['origin_url'] as String,
+      image: data['image'] as String,
+      episodes: (jsonDecode(data['episodes'] as String) as List)
+          .map((e) => e as String)
+          .toList(),
+      url: data['url'] as String,
+      created: data['created'] as String,
+      cachedAt: data['cached_at'] as int,
+    );
+  }
+}
