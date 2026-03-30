a7b52bc51f6773e5bcc4b468e1e7204ad8dc4467
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 13:26:49 +0300

    feat(data/api): added RickAndMortyApiRepositoryImpl

---
+++ lib/data/api/rick_and_morty_api_repository_impl.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:rick_and_morty/data/%20implements/rick_and_morty_api_repository.dart';
+import 'package:rick_and_morty/data/models/character/character.dart';
+import 'package:rick_and_morty/data/models/full_character/full_character.dart';
+
+class RickAndMortyApiRepositoryImpl implements RickAndMortyApiRepository {
+  final Dio _dio;
+
+  RickAndMortyApiRepositoryImpl({required Dio dio}) : _dio = dio;
+
+  @override
+  Future<FullCharacter> getAll() async {
+    try {
+      final Response response = await _dio.get('/character');
+      return FullCharacter.fromJson(response.data);
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout) {
+        '`${e.message} : ${e.error.toString()}';
+      }
+      if (e.type == DioExceptionType.badResponse) {
+        e.message;
+      }
+      if (e.type == DioExceptionType.connectionTimeout) {
+        e.message;
+      }
+      // if (e.type == DioExceptionType.unknown) {
+      //   HandleException.cacheError(statusCode: );
+      // }
+      print(e.message);
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Character> getFromId(int id) async {
+    try {
+      final Response response = await _dio.get('/character/$id');
+      return Character.fromJson(response.data);
+    } on DioException catch (e) {
+      print(e.message);
+      rethrow;
+    }
+  }
+}
