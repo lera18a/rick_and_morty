4a29dad472dbcc105a2b4d47df0aefa9578f9e5b
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 14:16:27 +0300

    feat(core/widgets): added ErrorLoc

---
+++ lib/core/widgets/error_loc.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ErrorLoc extends StatelessWidget {
+  const ErrorLoc({super.key, required this.message});
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.error, color: Colors.red, size: 60),
+          const SizedBox(height: 16),
+          Text(
+            message,
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
