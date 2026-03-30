e0b33e2822d1ea0f3c8138d58e70b975b91fd513
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 14:25:57 +0300

    feat(core/di): added BlocInjector

---
+++ lib/core/di/bloc_injector.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:rick_and_morty/presenter/cubit/characters/characters_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BlocInjector extends StatelessWidget {
+  const BlocInjector({super.key, required Widget child}) : _child = child;
+
+  final Widget _child;
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          CharactersCubit(characterRepository: context.read())..getCharacter(),
+      child: _child,
+    );
+  }
+}
