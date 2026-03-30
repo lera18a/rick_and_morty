285fb33da89b1edd8f473cdb8038e1a6dd44bf5a
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 13:10:29 +0300

    refactor: update apiTest

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,17 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rick_and_morty/core/enviroment_variables.dart';
-import 'package:rick_and_morty/data/api/api_repo.dart';
+import 'package:rick_and_morty/data/api/rick_and_morty_api_repository_impl.dart';
 
 void main() {
-  final apiClient = ApiRepo(host: EnviromentVariables.apiHost);
+  final dio = Dio(BaseOptions(baseUrl: EnviromentVariables.host));
+  final apiClient = RickAndMortyApiRepositoryImpl(dio: dio);
 
   group('Get characters group', () {
     test('Get characters result', () async {
-      final response = await apiClient.getCharacters();
+      final response = await apiClient.getAll();
       expect(response.results.isNotEmpty, true);
     });
     test('Get characters info', () async {
-      final response = await apiClient.getCharacters();
+      final response = await apiClient.getAll();
       expect(response.info.pages != 0, true);
     });
   });
