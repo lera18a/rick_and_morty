9d0c7ee9eec4de6e0b14e8e0e3ad7fc1be7f9fd1
Badrakova Valeria <lera_18a@mail.ru>
2026-03-30 17:08:15 +0300

    refactor(core/widgets): rename ListCardsWidget to DetailCardWidget

---
+++ lib/core/widgets/details_card_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty/core/widgets/info_widget.dart';
+import 'package:rick_and_morty/core/widgets/status_of_character_widget.dart';
+
+class DetailCardWidget<T> extends StatelessWidget {
+  const DetailCardWidget({
+    super.key,
+    required this.lastLocationPlace,
+    required this.firstLocationPlace,
+    required this.status,
+    required this.species,
+    required this.name,
+    required this.imageURL,
+    required this.character,
+  });
+  final T character;
+  final String lastLocationPlace;
+  final String firstLocationPlace;
+  final String status;
+  final String species;
+  final String name;
+  final String imageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black54,
+      child: Column(
+        crossAxisAlignment: .start,
+        mainAxisSize: .min,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: Image.network(imageURL, fit: BoxFit.cover),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: .start,
+              mainAxisSize: .min,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
+                  ),
+                ),
+                StatusOfCharacterWidget.alive(status: status, species: species),
+                InfoWidget.lastLocation(place: firstLocationPlace),
+                InfoWidget.firstLocation(place: lastLocationPlace),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
